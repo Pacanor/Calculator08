a9206517deccc0fab7f3c1a6b8ff0cd7dcc99f63
pacanor <papiasb@gmail.com>
2020-01-10 11:46:12 +0200

    Feature: Added Standard deviation functions and interface

---
+++ StatOperationsTests/StatStandardDevTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatOperations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatOperations.Tests
+{
+    [TestClass()]
+    public class StatStandardDevTests
+    {
+        [TestMethod()]
+        public void StandDevIntTest()
+        {
+            int[] values = { 1, 2, 3, 4, 5 };
+            var standDev = StatStandardDev.StandDev(values);
+            Assert.AreEqual(1.41421, Helpers.Rounding.RoundFiveDecimals(standDev));
+        }
+
+    }
+}

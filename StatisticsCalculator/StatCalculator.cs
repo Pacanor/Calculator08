2422e0b2a54e1c2e5e14c75f4e786409fbce949b
pacanor <papiasb@gmail.com>
2020-01-10 11:47:06 +0200

    Feature: Added standard deviation to the statistic calculator

--- StatisticsCalculator/StatCalculator.cs
+++ StatisticsCalculator/StatCalculator.cs
@@ -1,15 +1,22 @@
 ﻿using BasicCalculator;
 using StatMean;
+using StatStandardDev;
 
 namespace StatisticsCalculator
 {
-    public class StatCalculator : Calculator, IFindMean
+    public class StatCalculator : Calculator, IFindMean, IFindStandDev
     {
         private readonly FindMean mean = new FindMean();
+        private readonly FindStandDev standDev = new FindStandDev();
 
         public dynamic Mean(dynamic values)
         {
             return mean.Mean(values);
+        }
+
+        public double StandDev(dynamic values)
+        {
+            return standDev.StandDev(values);
         }
     }
 }
